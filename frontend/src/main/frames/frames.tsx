0d03223da98b5d4a07632996947a0915cddb319f
JOMI195 <johannes.middelberg@gmx.de>
2025-02-16 22:08:13 +0100

    Update frames.tsx

--- frontend/src/main/frames/frames.tsx
+++ frontend/src/main/frames/frames.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Container } from "@mui/material";
 import { AddButton } from "./buttons/addButton";
 import Dialogs from "./dialogs/dialogs";
 import FramesGallery from "./gallery/framesGallery";
+import { useAppDispatch } from "@/store";
+import { fetchframes } from "@/store/entities/frames/frames.actions";
 
 const Frames: React.FC = () => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchframes());
+    }, []);
+
     return (
         <Container maxWidth={"md"} disableGutters>
             <Box sx={{
