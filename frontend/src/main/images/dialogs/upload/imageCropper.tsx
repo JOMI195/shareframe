0716a58d662aab0848cdae51f2a4743c28847b24
JOMI195 <johannes.middelberg@gmx.de>
2025-02-17 13:24:30 +0100

    Update imageCropper.tsx

--- frontend/src/main/images/dialogs/upload/imageCropper.tsx
+++ frontend/src/main/images/dialogs/upload/imageCropper.tsx
@@ -2,34 +2,39 @@ import React, { useEffect, useRef } from 'react';
 import { FixedCropperRef, FixedCropper, ImageRestriction } from 'react-advanced-cropper';
 import 'react-advanced-cropper/dist/style.css';
 import './cropper.css';
+import 'react-advanced-cropper/dist/themes/corners.css';
 
-interface IImageCropperProps {
+interface CropperWrapperProps {
     image: File;
-    setCropper: React.Dispatch<React.SetStateAction<FixedCropperRef | null>>
+    setCropper: React.Dispatch<React.SetStateAction<FixedCropperRef | null>>;
 }
 
-const ImageCropper: React.FC<IImageCropperProps> = ({ image, setCropper }) => {
+const ImageCropper: React.FC<CropperWrapperProps> = ({ image, setCropper }) => {
     const cropperRef = useRef<FixedCropperRef>(null);
 
     useEffect(() => {
         setCropper(cropperRef.current);
-    }, [cropperRef])
+    }, [cropperRef, setCropper]);
 
     return (
         <FixedCropper
             ref={cropperRef}
-            className="cropper"
             src={URL.createObjectURL(image)}
+            className="cropper"
             stencilProps={{
                 handlers: false,
-                lines: false,
+                lines: true,
                 movable: false,
-                resizable: false
+                resizable: false,
             }}
             stencilSize={{ width: 800, height: 480 }}
-            imageRestriction={ImageRestriction.stencil}
+            imageRestriction={ImageRestriction.none}
+            transformImage={{
+                adjustStencil: false,
+            }}
+            checkOrientation={true}
         />
-    )
-}
+    );
+};
 
 export default ImageCropper;
