18213d6d4ee3030d1acaa00db395cc1be408d5e1
JOMI195 <johannes.middelberg@gmx.de>
2025-02-22 16:44:32 +0100

    Update urls.py

--- backend/frame_updates/urls.py
+++ backend/frame_updates/urls.py
@@ -4,17 +4,17 @@ from .views import UpdateAPIViewSet
 URL_PATH = "frame-updates/"
 urlpatterns = [
     path(
-        f"{URL_PATH}versions",
+        f"{URL_PATH}versions/",
         UpdateAPIViewSet.as_view({"get": "list_versions"}),
         name="version-list",
     ),
     path(
-        f"{URL_PATH}latest",
+        f"{URL_PATH}latest/",
         UpdateAPIViewSet.as_view({"get": "get_latest"}),
         name="latest-version",
     ),
     path(
-        f"{URL_PATH}<str:version>",
+        f"{URL_PATH}<str:version>/",
         UpdateAPIViewSet.as_view({"get": "get_version"}),
         name="version-detail",
     ),
