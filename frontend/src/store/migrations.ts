b74a3c85967f4de6079f597ba70be7d6d1bf1a75
JOMI195 <johannes.middelberg@gmx.de>
2025-02-27 23:49:20 +0100

    Migrate redux store

--- frontend/src/store/migrations.ts
+++ frontend/src/store/migrations.ts
@@ -2,11 +2,13 @@ import { MigrationManifest } from 'redux-persist';
 import migration2 from './migrations/migration2';
 import migration3 from './migrations/migration3';
 import migration4 from './migrations/migration4';
+import migration5 from './migrations/migration5';
 
 const typedMigrations = {
     2: migration2,
     3: migration3,
     4: migration4,
+    5: migration5
 };
 
 const migrations = typedMigrations as unknown as MigrationManifest;
