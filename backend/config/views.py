87a238e5f7fa816e13463b9af21628c2f5d21637
JOMI195 <johannes.middelberg@gmx.de>
2025-02-27 23:40:00 +0100

    Update views.py

--- backend/config/views.py
+++ backend/config/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.db.models import Q
 
 from frame_updates.authentication import FrameTokenAuthentication
-from images.models import Image
+from images.models import Image, ImageVariant
 from sent_images.models import SentImage
 
 
@@ -29,29 +29,7 @@ class MediaAccessView(APIView):
         if user.is_staff:
             access_granted = True
         else:
-            images: List[Image] = Image.objects.all()
-            for image in images:
-                doc_path = image.image.url
-                doc_path = doc_path.replace(settings.MEDIA_URL + "private/", "")
-                if path.endswith("/"):
-                    doc_path = doc_path + "/"
-
-                if path == doc_path:
-                    if image.user == user:
-                        access_granted = True
-                        break
-
-                    sentImage_exists = SentImage.objects.filter(
-                        (
-                            (Q(sender=user) & Q(reciever=image.user))
-                            | (Q(sender=image.user) & Q(reciever=user))
-                        )
-                        & Q(image=image)
-                    ).exists()
-
-                    if sentImage_exists:
-                        access_granted = True
-                        break
+            access_granted = self._check_path_access(path, user)
 
         if access_granted:
             response = Response(status=200)
@@ -60,6 +38,57 @@ class MediaAccessView(APIView):
             return response
         else:
             raise PermissionDenied("Media not found or not authorized to access.")
+
+    def _normalize_path(self, url_path):
+        """Normalize a media URL to a comparable path"""
+        normalized = url_path.replace(settings.MEDIA_URL + "private/", "")
+        return normalized
+
+    def _check_user_access(self, owner, user, image):
+        """Check if user has access to image owned by owner"""
+        # Direct ownership
+        if owner == user:
+            return True
+
+        # Check if the image was sent to/from this user
+        sentImage_exists = SentImage.objects.filter(
+            (
+                (Q(sender=user) & Q(reciever=owner))
+                | (Q(sender=owner) & Q(reciever=user))
+            )
+            & Q(image=image)
+        ).exists()
+
+        return sentImage_exists
+
+    def _check_path_access(self, path, user):
+        """Check if user has access to the given path"""
+        # First check original images
+        images = Image.objects.all()
+        for image in images:
+            doc_path = self._normalize_path(image.image.url)
+
+            if path.endswith("/"):
+                doc_path = doc_path + "/"
+
+            if path == doc_path:
+                if self._check_user_access(image.user, user, image):
+                    return True
+
+        # Then check variants
+        variants = ImageVariant.objects.all()
+        for variant in variants:
+            doc_path = self._normalize_path(variant.file.url)
+
+            if path.endswith("/"):
+                doc_path = doc_path + "/"
+
+            if path == doc_path:
+                parent_image = variant.parent_image
+                if self._check_user_access(parent_image.user, user, parent_image):
+                    return True
+
+        return False
 
 
 class FrameUpdatesAccessView(APIView):
