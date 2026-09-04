76bfb1163bdd64cd0959809fac8909823e772453
JOMI195 <johannes.middelberg@gmx.de>
2026-09-04 20:39:01 +0200

    decrease jwt token lifetime

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -422,7 +422,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
